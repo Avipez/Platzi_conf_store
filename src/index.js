42c3fa9e8791bad5568115973d04d8e20d4a2d7a
David Lopez <dg_lf@hotmail.com>
2022-05-17 21:52:48 -0500

    Avances hasta clase 16

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/app"
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+/* const root = ReactDOM.createRoot(document.getElementById("app"));
+
+root.render(<App />) */
