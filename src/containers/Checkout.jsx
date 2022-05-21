bc195295d3631ae08cd98a3a4505079a7b734569
David Lopez <dg_lf@hotmail.com>
2022-05-21 18:40:23 -0500

    avances hasta clase 21

---
+++ src/containers/Checkout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cheackout = () => {
+    return (
+        <h1>Cheackout</h1>
+    );
+}
+
+export default Cheackout;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/app"
+import App from './routes/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 /* const root = ReactDOM.createRoot(document.getElementById("app"));
 
